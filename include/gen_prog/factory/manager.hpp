4355f483bf146dc6ce9c50eab2ed312618d5b19a
David Callu <callu.david@gmail.com>
2015-12-07 16:43:45 +0100

    feat(factory): add manager and proxies

---
+++ include/gen_prog/factory/manager.hpp
@@ -0,0 +1,66 @@
+#ifndef GEN_PROG__FACTORY__MANAGER_HPP_
+#define GEN_PROG__FACTORY__MANAGER_HPP_
+
+
+
+#include <unordered_map>
+
+#include <boost/functional/factory.hpp>
+#include <boost/function.hpp>
+
+
+
+namespace gp {
+namespace factory {
+
+template <typename T>
+using factory_generator = boost::function<T*()>;
+
+template <typename IdentifierT, typename ValueT, typename PrototypeT = boost::function<ValueT*()> >
+class manager
+{
+public:
+    using id_type           = IdentifierT;
+    using value_type        = ValueT;
+    using factory_prototype = PrototypeT;
+
+    using factory_map       = std::unordered_map<id_type, factory_prototype>;
+    using result_type       = typename factory_prototype::result_type;
+
+
+public:
+    bool add(const id_type & id, const factory_prototype & factory)
+    {
+        return _map.insert(std::make_pair(id, factory)).second;
+    }
+
+    void remove(const id_type & id)
+    {
+        auto it = _map.find(id);
+        if (it != _map.end()) _map.erase(it);
+    }
+
+    template <typename... Args>
+    result_type create(const id_type & id, Args&&... args) const
+    { return get(id)(std::forward<Args>(args)...); }
+
+
+private:
+    factory_prototype get(const id_type & id) const
+    {
+        auto it = _map.find(id);
+        if (it != _map.end()) return it->second;
+        else return factory_prototype();
+    }
+
+private:
+    factory_map _map;
+};
+
+
+}  // namespace factory
+}  // namespace gp
+
+
+#endif // ** GEN_PROG__FACTORY__MANAGER_HPP_ ** //
+// End of file
---
+++ include/gen_prog/factory/proxy.hpp
@@ -0,0 +1,53 @@
+#ifndef GEN_PROG__FACTORY__PROXY_HPP_
+#define GEN_PROG__FACTORY__PROXY_HPP_
+
+
+
+#include <string>
+#include <unordered_map>
+
+#include <boost/functional/factory.hpp>
+#include <boost/noncopyable.hpp>
+
+
+
+namespace gp
+{
+namespace factory
+{
+
+namespace detail {
+
+template <typename T>
+using factory_generator = boost::factory<T>;
+
+} // namespace detail
+
+template <typename ManagerT, typename ValueT, template <typename> class FactoryGeneratorT = detail::factory_generator >
+class proxy : public boost::noncopyable
+{
+public:
+    using manager_type = ManagerT;
+    using value_type   = ValueT;
+
+    template <typename T>
+    using factory_generator = FactoryGeneratorT<T>;
+
+    using factory_type = factory_generator<value_type>;
+    using id_type      = typename manager_type::id_type;
+
+    proxy(const id_type & id) : _id(id)
+    { manager_type::instance().add( _id, factory_type() ); }
+    ~proxy()
+    { manager_type::instance().remove( _id ); }
+
+private:
+    id_type _id;
+};
+
+
+} // namespace factory
+} // namespace gp
+
+#endif // ** GEN_PROG__FACTORY__PROXY_HPP_ ** //
+// End of file
